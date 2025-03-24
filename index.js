440f574ba8f4d3bede327d17d2ca1c45d8e137e3
Jahongir Yahshimurotov <jahongiryahshimurotov@gmail.com>
2025-03-24 14:28:47 +0500

    expressjs-v2

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
-import express from "express";
-import { create } from "express-handlebars";
+// import express from "express";
+// import { create } from "express-handlebars";
 
+// const app = express();
+
+// const hbs = create({
+// 	defaultLayout: "main",
+// 	extname: "hbs",
+// });
+
+// app.engine('hbs', hbs.engine);
+// app.set('view engine', 'hbs');
+// app.set('views', './views');
+
+
+// app.get("/", (req, res) => {
+// 	res.render("index");
+// });
+
+// app.get("/about" , (req, res) => {
+// 	res.render("about");
+// });
+
+// const PORT = process.env.PORT || 4000
+// app.listen(PORT, () => console.log(`Server is running... ${PORT}`));
+
+import express from "express";
+import {create} from "express-handlebars";
 const app = express();
 
 const hbs = create({
 	defaultLayout: "main",
 	extname: "hbs",
+ });
+
+app.engine("hbs", hbs.engine);
+app.set("view engine", 'hbs');
+app.set("views", "./views");
+
+const PORT = process.env.PORT || 4100
+app.listen(PORT, () => {
+	console.log(`Server is running ${PORT}`);
 });
-
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
-app.set('views', './views');
-
 
 app.get("/", (req, res) => {
 	res.render("index");
 });
 
-app.get("/about" , (req, res) => {
+app.get("/about", (req, res) => {
 	res.render("about");
 });
 
-const PORT = process.env.PORT || 4000
-app.listen(PORT, () => console.log(`Server is running... ${PORT}`));
+// 2 - O'zim yozgan qo'd express JS mustahkamlash uchun

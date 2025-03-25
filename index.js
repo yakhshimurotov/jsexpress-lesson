73378a90ba133c701d8b8d3e8caec8c81929fedc
Jahongir Yahshimurotov <jahongiryahshimurotov@gmail.com>
2025-03-25 15:48:44 +0500

    expressjs-v3-login

--- index.js
+++ index.js
@@ -26,6 +26,8 @@
 
 import express from "express";
 import {create} from "express-handlebars";
+import AouthRoutse from "./routse/auth.js";
+import ProductsRoutse from "./routse/products.js";
 const app = express();
 
 const hbs = create({
@@ -37,17 +39,12 @@ app.engine("hbs", hbs.engine);
 app.set("view engine", 'hbs');
 app.set("views", "./views");
 
+app.use(AouthRoutse);
+app.use(ProductsRoutse);
+
 const PORT = process.env.PORT || 4100
 app.listen(PORT, () => {
 	console.log(`Server is running ${PORT}`);
 });
 
-app.get("/", (req, res) => {
-	res.render("index");
-});
-
-app.get("/about", (req, res) => {
-	res.render("about");
-});
-
-// 2 - O'zim yozgan qo'd express JS mustahkamlash uchun
+// 3 - O'zim yozgan qo'd express JS mustahkamlash uchun

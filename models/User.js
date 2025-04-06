2cc41ca1dab619bdf283a19b4e392a936da85066
Jahongir Yahshimurotov <jahongiryahshimurotov@gmail.com>
2025-04-06 18:42:36 +0500

    expressjs-v7-login

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 import { model, Schema } from "mongoose";
 const userSchema = Schema({
-    firstName: {type: String, required: true},
-    lastName: {type: String, required: true},
+    FirstName: {type: String, required: true},
+    LastName: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true}
 });
--- routse/auth.js
+++ routse/auth.js
@@ -1,42 +1,35 @@
 import { Router } from "express";
-import User from "../models/User";
+import User from "../models/user.js";
 const router = Router();
 
 router.get("/login", (req, res) => {
-    res.render("login", {
-        isLogin: true,
-    });
+  res.render("login", {
+    isLogin: true,
+  });
 });
 
 router.post("/login", (req, res) => {
-    console.log(req.body);
-    res.redirect("/");
+  console.log(req.body);
+  res.redirect("/");
 });
 
 router.get("/sign-up", (req, res) => {
-    res.render("sign-up", {
-        isSignup: true,
-    });
+  res.render("sign-up", {
+    isSignup: true,
+  });
 });
 
 router.post("/sign-up", async (req, res) => {
-    try {
-        const userDate = {
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            email: req.body.email,
-            password: req.body.password,
-        };
-        console.log(userDate);
-
-        const user = await User.create(userDate);
-        console.log(user);
-
-        res.redirect("/");
-    } catch (error) {
-        console.error("Xatolik yuz berdi:", error);
-        res.status(500).send("Serverda xatolik yuz berdi");
-    }
+  const userDate = {
+    FirstName: req.body.FirstName,
+    LastName: req.body.LastName,
+    email: req.body.email,
+    password: req.body.password,
+  }
+  console.log(userDate);
+  const user = await User.create(userDate);
+  console.log(user);
+  res.redirect("/");
 });
 
 export default router;
